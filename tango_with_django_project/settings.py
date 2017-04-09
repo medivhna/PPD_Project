c617ffecf0859a622e5c22e86ea5e8572dc25a36
Medivhna <teljerry@live.cn>
2017-04-10 00:47:00 +0800

    'D1'

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_PATH = os.path.join(BASE_DIR,'static')
 
 STATIC_URL = '/static/'
